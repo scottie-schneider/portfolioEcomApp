ba044e877354b553f590d27c2cb1f2f7d7185598
scottie-schneider <scott@laclave.co>
2020-01-13 16:18:05 -0500

    adding in images to menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({title}) => {
+const MenuItem = ({title, imageUrl}) => {
   return (
-    <div className="menu-item">
+    <div 
+      className="menu-item"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    >
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
