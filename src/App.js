1d976367562d138cc52f32f068109054787c898d
scottie-schneider <scott@laclave.co>
2020-01-13 17:33:24 -0500

    adding in a collection preview item, and mapping over the data

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h2>Hats page</h2>
-  </div>
-)
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     )
   }
