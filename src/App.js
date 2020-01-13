1189054db0ba03706694a7691bccba298209b98b
scottie-schneider <scott@laclave.co>
2020-01-13 18:47:03 -0500

    created logo, header and added to app

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-      </Switch>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
     )
   }
 }
