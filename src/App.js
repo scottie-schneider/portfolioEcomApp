8d7be91bfd8268b5cabe06ae87c41d2f26079080
scottie-schneider <scott@laclave.co>
2020-01-13 16:51:38 -0500

    adding in home route, testing secondary page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h2>Hats page</h2>
+  </div>
+)
 class App extends Component {
   render() {
-    return <HomePage />
+    return (
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </div>
+    )
   }
 }
 
