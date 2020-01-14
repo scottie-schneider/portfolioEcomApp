e63b3904c9dfca94f0b886e0f01df9f85e31a3df
scottie-schneider <scott@laclave.co>
2020-01-14 12:46:33 -0500

    add authentication event handlers in App JS

--- src/App.js
+++ src/App.js
@@ -7,8 +7,26 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import { auth } from './firebase/firebase.utils';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+      console.log(user)
+    })
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
   render() {
     return (
       <div>
