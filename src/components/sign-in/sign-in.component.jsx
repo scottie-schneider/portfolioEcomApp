053d9d56564ce475056ce24a97f95b9aabc7468b
scottie-schneider <scott@laclave.co>
2020-01-13 20:01:21 -0500

    add forminput component and label transitions

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component.jsx';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -26,10 +28,22 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input name="email" type="email" value={this.state.email} required onChange={this.handleChange}/>
-          <label>Email</label>
-          <input name="password" type="password" value={this.state.password} required onChange={this.handleChange} />
-          <label>Password</label>
+          <FormInput 
+            name="email" 
+            type="email" 
+            value={this.state.email} 
+            required 
+            label="email"
+            handleChange={this.handleChange}
+          />
+          <FormInput 
+            name="password" 
+            type="password" 
+            value={this.state.password} 
+            required 
+            label="password"
+            handleChange={this.handleChange} 
+          />
           <input type="submit" value="Submit Form" />
         </form>
       </div>
